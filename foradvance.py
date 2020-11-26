47d4949ee2fc58d67e1fd7719a755962291ca368
Sử Thành Công <stc17022002@gmail.com>
2020-11-26 16:13:27 +0700

    first commit

--- foradvance.py
+++ foradvance.py
@@ -1,18 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov 26 00:16:45 2020
-
-@author: ADMIN
-"""
-
 list2 = [({'name': 'Peter', 'age':2}, {'name': 'John', 'age':21}), 
          ({'name': 'Mary', 'age':2}, {'name': 'Trandanpro', 'age':21}), 
          ({'name': 'Nam', 'age':2}, {'name': 'Hung', 'age':21}), 
          ({'name': 'Mai', 'age':2}, {'name': 'Loan', 'age':21})]
 
-# 4tuple
-# 2 phần tử dic trong tuple
-
+#List2 gồm 4 Tuple
+#Tuple gồm 2 Dic
+#Dic gồm 2 key: name , age
 
 for index,tup in enumerate(list2,1):
     print(index,tup)
