cd2b563799f198661bfac2e6ec3c36c3b9ca35eb
Sử Thành Công <stc17022002@gmail.com>
2020-11-27 21:03:32 +0700

    alabama

--- Bai12.2.py
+++ Bai12.2.py
@@ -1,9 +1,13 @@
 import random
 import string
-n = random.randint(1,20)
+
+#Tạo giá trị cho name
+n = random.randint(1,15)
 name = random.choice(string.ascii_uppercase)
 for i in range(n):
     name += random.choice(string.ascii_lowercase)
+    
+#Tạo giá trị ngẫu nhiên cho age
 n = random.randint(0, 150)
 dic = {'name':name,'age':n}
-print(dic)
+print('Dictionary vừa tạo:',dic)
--- Bai12.3.py
+++ Bai12.3.py
@@ -1,17 +1,22 @@
 import random
 import string
-n = random.randint(50, 100) #Tạo ngẫu nhiên số phần tử có trong mỗi list
+
+n = random.randint(50, 100) #Tạo ngẫu nhiên số phần tử ditionary có trong list
 print('Số phần tử trong mỗi list hiện tại:',n)
-print('''
-            ''')
+
 list1 = []
-dic = {}
+
 for i in range(n):
+    
+    #Tạo giá trị ngẫu nhiên cho name: (chữ cái đầu tiên in hoa)
     n = random.randint(1,15)
-    name = random.choice(string.ascii_uppercase)
+    name = random.choice(string.ascii_uppercase) 
     for i in range(n):
         name += random.choice(string.ascii_lowercase)
-    n = random.randint(0, 150)
-    dic = {'name':name,'age':n}
-    list1.append(dic)         #Thêm phần tử vừa tạo vào list
-print(list1)
+        
+    n = random.randint(0, 150)  #Tạo giá trị ngẫu nhiên cho age
+    
+    dic = {'name':name,'age':n} #Tạo dictionary từ 2 giá trị name và age vừa tạo
+    list1.append(dic)         #Thêm phần tử dictionary vừa tạo vào list
+    
+print('List =',list1)
